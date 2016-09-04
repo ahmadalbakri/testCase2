f5ead0ec460d0068aa1de7d9a504c856934225f2
Ahmad Albakri Zabri <greenjeli@gmail.com>
2016-09-04 14:22:55 +0800

    add in bunch of stuff, code cleanup

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ The settings will be updated on checkout (push into the object).
 
 This object will become a reference to calculate the pricing.
 
-Please install dependencies -> bower install
+#How it works
+1) instantiate checkout
+2) push setting into object created on 1
+3) add more item into object by invoking .add() method
+4) calculate total, together with pricing rules on .total() method
+
+#Notes
+1) Please install dependencies -> bower install
+2) For better experience, use vue.js 2 way binding to summarize purchase
--- js/main.js
+++ js/main.js
@@ -1,55 +1,41 @@
-    var ads = []; var pricingRules = [];
-
     switch (company.id) {
         case 1:
             name = "Unilever";
-            settings = settings[0];
+            settings = data.settings[0];
             break;
         case 2:
             name = "Apple";
-            settings = settings[1];
+            settings = data.settings[1];
             break;
         case 3:
             name = "Nike";
-            settings = settings[2];
+            settings = data.settings[2];
             break;
         case 4:
             name = "Ford";
-            settings = settings[3];
+            settings = data.settings[3];
             break;
     };
-    pricingRules.push(settings);
+    var pricingRules = [];
+    // push company setting into array
+    pricingRules.push(data.settings);
 
     function checkout(pricingRules) {
         this.add = function(item) {
-            //d.add(1)
-            pricingRules.push(item);
-        },    
-        this.total = function() {
-            // get ads count from each ads
-            // var d = {"id":1, "count": 3};
-            $.each(pricingRules, function(index, value) {
-                console.log(value);
-            });
-        }
+                pricingRules.push(item);
+            },
+            this.total = function() {
+                // logic and total calculation of the ads
+                $.each(pricingRules, function(index, value) { // var d = {"id":1, "count": 3};
+                    console.log(value);
+                });
+            }
     };
 
-    // instantiate checkout
+    // Instantiate checkout
     var d = new checkout(pricingRules);
-    
-    $(document).ready(function() {        
-        $('#companyName').append(name); // append company Name
 
-        $.each(adsDetail, function(index, value) {
-            $("#cart").append("<li class='adsDetails'> \
-            <div>" + value.title + "</div> \
-            <div>" + value.price + "</div> \
-            <div>\
-                <input type='button' onclick='decrementValue(" + index + ")' value='-' />\
-                <input type='text' value=1 maxlength=2 max=10 size=1 id='number" + index + "'/>\
-                <input type='button' onclick='incrementValue(" + index + ")' value='+' />\
-                <input type='button' onclick='d.add(" + index + ", 3)' value='Add' />\
-            </div> \
-        </li>");
-        });     
+    $(document).ready(function() {
+        // append company Name
+        $('#companyName').append(name);
     });
